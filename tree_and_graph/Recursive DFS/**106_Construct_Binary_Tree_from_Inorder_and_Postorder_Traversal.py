3d8649ac150db41c9c4d3839c0e96f330fb9ef55
Zhenmei Chen <zhechen@Zhenmeis-Air.fios-router.home>
2024-11-13 09:30:52 -0500

    11/11

--- tree_and_graph/Recursive DFS/**106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
+++ tree_and_graph/Recursive DFS/**106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -26,11 +26,13 @@ Return the following binary tree:
    15   7
    """
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution(object):
     def buildTree(self, inorder, postorder):
         """
